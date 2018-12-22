f3bf55cccf5bfb1d0ae262acbd84b67e359d4ca0
peitur <peitur@gmail.com>
2018-12-22 09:49:54 +0100

    skel

---
+++ lib/pyvbcc/cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os, sys, re
+import json
+
+from pprint import pprint
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ lib/pyvbcc/utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os, sys, re
+from pprint import pprint
+
+
+
+if __name__ == "__main__":
+    pass
