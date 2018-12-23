42d4b6533883f8db3e7daedfa2bbc5f6cc0f6a57
peitur <peitur@gmail.com>
2018-12-23 11:21:55 +0100

    cleanup, removed unneeded dowload function

--- lib/pyvbcc/utils.py
+++ lib/pyvbcc/utils.py
@@ -3,7 +3,6 @@
 import os, sys, re
 import json
 import hashlib
-import requests
 from pprint import pprint
 
 from pathlib import Path
@@ -139,52 +138,6 @@ def read_env( key ):
     return os.environ.get( key )
 
 ################################################################################
-
-
-################################################################################
-## HTTP operations, download large files
-################################################################################
-
-def download_file( name, url_filename, local_filename, **opt ):
-    x_size = 0
-    l_size = 0
-    r_size = 0
-    bsize=1024
-    overwrite = False
-    timeout = 10
-    debug = False
-
-    if 'debug' in opt: debug = opt['debug']
-    if 'bsize' in opt: bsize = opt['bsize']
-    if 'timeout' in opt: timeout = opt['timeout']
-
-    if 'overwrite' in opt and opt['overwrite'] in (True,False):
-        overwrite = opt['overwrite']
-
-    if Path( local_filename ).exists():
-        l_size = Path( local_filename ).stat().st_size
-
-    r = requests.get( url_filename, timeout=timeout, stream=True)
-    if 'content-length' in r.headers:
-        r_size = r.headers['content-length']
-
-    if debug:
-        pprint( r.headers )
-
-    if r.status_code != 200:
-        print("# ERROR: Could not find %s,  %s : " % ( url_filename, r.status_code ) )
-        return None
-
-    if not Path( local_filename ).exists() or overwrite:
-        with open( local_filename, 'wb') as f:
-            for chunk in r.iter_content( chunk_size=bsize ):
-                if chunk: # filter out keep-alive new chunks
-                    x_size += len( chunk )
-                    f.write(chunk)
-
-    r.close()
-
-    return local_filename
 
 
 
