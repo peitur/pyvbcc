7c9a42950fc597de3150a5565038584e22b346f4
peitur <peitur@gmail.com>
2019-01-03 13:17:31 +0100

    wip, adding install iso

--- lib/pyvbcc/command.py
+++ lib/pyvbcc/command.py
@@ -4,6 +4,7 @@ import os, sys, re
 import subprocess, shlex
 
 import pyvbcc
+import pyvbcc.utils
 
 from pprint import pprint
 
@@ -255,16 +256,17 @@ class AttachDiskCommand( GenericCommand ):
 
         params = ["VBoxManage",
             "storageattach", self._vm,
-            "--type", self._type,
             "--storagectl", self._controller,
             "--medium", self._disk
         ]
 
+        if pyvbcc.KEY_DISKS_TYPE in cfg: self._type = cfg[ pyvbcc.KEY_DISKS_TYPE ]
         if pyvbcc.KEY_DISKS_PORT in cfg: self._port = cfg[ pyvbcc.KEY_DISKS_PORT ]
         if pyvbcc.KEY_DISKS_MTYPE in cfg: self._mtype = cfg[ pyvbcc.KEY_DISKS_MTYPE ]
         if pyvbcc.KEY_DISKS_DEVICE in cfg: self._device = cfg[ pyvbcc.KEY_DISKS_DEVICE ]
         if pyvbcc.KEY_DISKS_COMMENT in cfg: self._comment = cfg[ pyvbcc.KEY_DISKS_COMMENT ]
 
+        if self._type: params += list( [ "--type", self._type ] )
         if self._port: params += list( [ "--port", self._port ] )
         if self._device: params += list( [ "--device", self._device ] )
         if self._mtype: params += list( [ "--mtype", self._mtype ] )
@@ -338,23 +340,27 @@ if __name__ == "__main__":
     import time
 
     info = ListSystemPropertiesCommand().run()
+    home = pyvbcc.utils.read_env("HOME")
 
     vm = "vm1"
     group = "test1"
     vmdir = "%s/%s/%s" % ( info["defaultmachinefolder"], group, vm )
     vmfile = "%s/%s.vmdk" % ( vmdir, vm )
+    dvdfile = "%s/Downloads/CentOS-7-x86_64-NetInstall-1810.iso" % ( home )
 
     vm1 = { pyvbcc.KEY_VM_NAME: vm, pyvbcc.KEY_GROUP_NAME: "test1", pyvbcc.KEY_VM_OSTYPE: "RedHat_64", pyvbcc.KEY_DISKS_FILE: vmfile }
     ctl0 = { pyvbcc.KEY_VM_NAME: vm, pyvbcc.KEY_CONTROLLER_NAME: "IDE", pyvbcc.KEY_CONTROLLER_TYPE: "ide", pyvbcc.KEY_CONTROLLER_PCOUNT: "2", pyvbcc.KEY_CONTROLLER_CHIPSET: "PIIX4", pyvbcc.KEY_CONTROLLER_BOOTABLE: "on" }
     ctl1 = { pyvbcc.KEY_VM_NAME: vm, pyvbcc.KEY_CONTROLLER_NAME: "SAS", pyvbcc.KEY_CONTROLLER_TYPE: "sas", pyvbcc.KEY_CONTROLLER_CHIPSET: "LSILogicSAS", pyvbcc.KEY_CONTROLLER_BOOTABLE: "on" }
     dks0 = { pyvbcc.KEY_DISKS_FILE: vmfile, pyvbcc.KEY_DISKS_FORMAT:"vmdk", pyvbcc.KEY_DISKS_NAME:"dm1-disk", pyvbcc.KEY_DISKS_SIZE: "4096"}
     atts0 = { pyvbcc.KEY_VM_NAME: vm, pyvbcc.KEY_CONTROLLER_NAME: "SAS", pyvbcc.KEY_DISKS_FILE: vmfile }
+    atts1 = { pyvbcc.KEY_VM_NAME: vm, pyvbcc.KEY_CONTROLLER_NAME: "IDE", pyvbcc.KEY_DISKS_FILE: dvdfile, pyvbcc.KEY_DISKS_TYPE: "dvddrive" }
 
     cli = CreateVmCommand( vm1 ).run()
     cli = CreateControllerCommand( ctl0 ).run()
     cli = CreateControllerCommand( ctl1 ).run()
     cli = CreateDiskCommand( dks0 ).run()
     cli = AttachDiskCommand( atts0 ).run()
+    cli = AttachDiskCommand( atts1 ).run()
     time.sleep(60)
     cli = DeleteVmCommand( vm1 ).run()
     pass
