b7d637842f83bcfb93c586db31dcd18da50c4ce1
peitur <peitur@gmail.com>
2019-05-30 22:56:43 +0200

    started preps for module split commands

---
+++ pyvbcc/vm/commands.py
@@ -0,0 +1,15 @@
+ #!/usr/bin/env python3
+
+import os, sys, re
+import json
+import getopt
+
+from pprint import pprint
+
+import pyvbcc
+import pyvbcc.config
+import pyvbcc.utils
+import pyvbcc.validate
+
+import pyvbcc.command
+
